df88d6ea71cd0e0086ba083c21e35b09b8bec345
NEDUNURIGANESH <bobbybablu001@gmail.com>
2022-09-18 19:16:31 +0530

    html data

---
+++ index.js
@@ -0,0 +1,55 @@
+const http=require("http");
+const fs=require("fs");
+let homecontent="";
+let projectcontent="";
+let registrationcontent="";
+fs.readFile("home.html",(err,home)=>{
+    if(err){ throw err};
+    homecontent=home;
+});
+fs.readFile("project.html",(err,project)=>{
+    if(err){
+        throw err;
+    }
+    projectcontent=project;
+});
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err;
+    }
+    registrationcontent=registration;
+});
+http.createServer((req,res)=>{
+    let url=req.url;
+    res.writeHeader(200,{"content-type":"type/html"});
+    switch(url)
+    {
+        case "/project":
+            res.write(projectcontent);
+            res.end();
+            break;
+        case "/registration":
+            res.write(registrationcontent);
+            res.end();
+            break;
+        default:res.write(homecontent);
+        res.end();
+        break;
+    }
+    /*
+    if(url=="/project")
+    {
+        res.write(projectcontent);
+            res.end();
+    }
+    else if(url=="/registration")
+    {
+        res.write(registrationcontent);
+            res.end();
+    }
+    else{
+        res.write(homecontent);
+        res.end();
+    }*/
+        
+}).listen(3000);
