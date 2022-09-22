6ea575e1eca355add9388b2c5bd0ec1c05fa6356
NEDUNURIGANESH <bobbybablu001@gmail.com>
2022-09-22 18:47:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const fs=require("fs");
 let homecontent="";
 let projectcontent="";
 let registrationcontent="";
-let args = require("minimist")(process.argv.slice(2));
+const args = require("minimist")(process.argv.slice(2));
 fs.readFile("home.html",(err,home)=>{
     if(err){ throw err};
-    homecontent=home;
+    homecontent=home.toString();
 });
 fs.readFile("project.html",(err,project)=>{
     if(err){
         throw err;
     }
-    projectcontent=project;
+    projectcontent=project.toString();
 });
 fs.readFile("registration.html",(err,registration)=>{
     if(err){
         throw err;
     }
-    registrationcontent=registration;
+    registrationcontent=registration.toString();
 });
 http.createServer((req,res)=>{
     let url=req.url;
-    res.writeHeader(200,{"content-type":"type/html"});
+    res.writeHead(200,{"content-type":"text/html"});
     switch(url)
     {
         case "/project":
@@ -37,22 +37,4 @@ http.createServer((req,res)=>{
         res.end();
         break;
     }
-    /*
-    if(url=="/project")
-    {
-        res.write(projectcontent);
-            res.end();
-    }
-    else if(url=="/registration")
-    {
-        res.write(registrationcontent);
-            res.end();
-    }
-    else{
-        res.write(homecontent);
-        res.end();
-    }*/
-        
-}).listen(args.port);
-
-
+}).listen(args["port"]);
